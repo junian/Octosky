513cb769112ce8e87b7f5a8a2c2466098ebdf2a9
Junian <junian.triajianto@gmail.com>
2013-11-20 14:32:10 +0700

    added Junian.Net plugins

---
+++ Assets/Plugins/Editor/TagClassGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class TagClassGenerator : EditorWindow {
+
+	[MenuItem ("Junian.Net/Generate Tag Class")]
+	static void Init () {
+		#if UNITY_EDITOR
+		string path = EditorUtility.SaveFilePanelInProject("Save Tag Class", "Tags", "cs", "Select save file path");
+		if(!string.IsNullOrEmpty(path))
+		{
+			string[] tags = UnityEditorInternal.InternalEditorUtility.tags;
+			//TextAsset asset = new TextAsset();
+			//asset.text = GenerateTagClass("Tags", tags);
+			//AssetDatabase.CreateAsset(asset, path);
+			File.WriteAllText(path, GenerateTagClass("Tags", tags));
+			AssetDatabase.Refresh();
+			EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath(path, typeof(Object)));
+		}
+		#endif
+	}
+
+	private static string GenerateTagClass(string className, string[] tags)
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.Append("public class ").Append(className).Append("\n{\n");
+
+		for(int i=0;i<tags.Length;i++)
+		{
+			sb.Append("    public const string ")
+				.Append(tags[i]).Append(" = ")
+				.Append("\"")
+				.Append(tags[i])
+				.Append("\";\n");
+		}
+
+		sb.Append("}\n");
+		return sb.ToString();
+	}
+}
