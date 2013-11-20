3edf8ce8437d6a7f469147fd9ccf2b3e62771ad1
Junian <junian.triajianto@gmail.com>
2013-11-20 15:00:07 +0700

    added enemy

---
+++ Assets/Scripts/LinearMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinearMove : MonoBehaviour {
+	public Vector2 speed =new Vector2(10.0f, 10.0f);
+	public Vector2 direction = new Vector2(-1.0f, 0.0f);
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = new Vector3(
+			speed.x * direction.x,
+			speed.y * direction.y,
+			0.0f);
+		movement = movement * Time.deltaTime;
+		transform.Translate(movement);
+	}
+}
