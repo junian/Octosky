39a670527e311dfd43031a4e4746700b8fae87a6
Junian <junian.triajianto@gmail.com>
2013-11-20 14:41:20 +0700

    added player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(50.0f, 50.0f);
+
+	// Use this for initialization
+	void Update () {
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(
+			speed.x * inputX,
+			speed.y * inputY,
+			0.0f);
+
+		movement = movement * Time.deltaTime;
+		transform.Translate(movement);
+	}
+}
