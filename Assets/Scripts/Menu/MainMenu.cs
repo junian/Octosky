ae0ea6e42c6de348faecfb50250079ec90a1912e
Junian <junian.triajianto@gmail.com>
2013-11-28 15:51:06 +0700

    added fb login

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -5,6 +5,51 @@ public class MainMenu : MonoBehaviour {
 
 	private GUISkin skin;
 
+	private void InitFacebook()
+	{
+		FbDebug.Log("Facebook init");
+
+		enabled = true;
+		if(FB.IsLoggedIn)
+		{
+			FbDebug.Log ("already login");
+			OnFBLoggedIn();
+		}
+	}
+
+	private void OnHideUnity(bool isGameShown)
+	{
+		FbDebug.Log("On Hide Unity");
+		if(!isGameShown)
+		{
+			Time.timeScale = 0.0f;
+		}
+		else
+		{
+			Time.timeScale = 1.0f;
+		}
+	}
+
+	void LoginCallback(FBResult result)
+	{
+		FbDebug.Log("Login callback");
+		if(FB.IsLoggedIn)
+		{
+			OnFBLoggedIn();
+		}
+	}
+
+	void OnFBLoggedIn()
+	{
+		FbDebug.Log("Logged in. ID: " + FB.UserId); 
+	}
+
+	void Awake()
+	{
+		enabled = false;
+		FB.Init(InitFacebook, OnHideUnity);
+	}
+
 	void Start()
 	{
 		skin = Resources.Load<GUISkin>("GUISkin");
@@ -12,18 +57,32 @@ public class MainMenu : MonoBehaviour {
 
 	void OnGUI()
 	{
-		const int buttonWidth = 84;
+		const int buttonWidth = 128;
 		const int buttonHeight = 60;
-		GUI.skin = skin;
-		if(GUI.Button(
-			new Rect(
-				(Screen.width - buttonWidth) / 2.0f, 
-				(2.0f * Screen.height / 3.0f) - (buttonHeight / 2.0f),
-		         buttonWidth,
-		         buttonHeight),
-			"Begin!"))
+		Rect rect = new Rect(
+			(Screen.width - buttonWidth) / 2.0f, 
+			(2.0f * Screen.height / 3.0f) - (buttonHeight / 2.0f),
+			buttonWidth,
+			buttonHeight);
+			
+		//GUI.skin = skin;
+		if(FB.IsLoggedIn)
 		{
-			Application.LoadLevel(Scenes.Game);
+			if(GUI.Button(
+				rect,
+				"Begin!"))
+			{
+				Application.LoadLevel(Scenes.Game);
+			}
+		}
+		else
+		{
+			if(GUI.Button(
+				rect,
+				"Facebook Login"))
+			{
+				FB.Login("email,publish_actions", LoginCallback);
+			}
 		}
 	}
 }
